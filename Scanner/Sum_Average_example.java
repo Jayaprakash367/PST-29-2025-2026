4cae60327111c484cb294c2f15e8dd8fb728ac18
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-21 20:55:38 +0530

    Add files via upload

---
+++ Scanner/Sum_Average_example.java
@@ -0,0 +1,20 @@
+package SCANNER;
+import java.util.Scanner;
+public class Sum_Average_example {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		 System.out.print("Enter the Size of the element:");
+		    int a=sc.nextInt();
+		    sc.nextLine();
+		    int arr[]=new int[a];
+		    System.out.println("Enter the number:");
+		    int Sum=0;
+		    for(int i=0; i<=a; i++) {
+		    	arr[i]=sc.nextInt(i);
+		    	Sum+=arr[i];
+		    			
+		    }
+	}
+
+}
