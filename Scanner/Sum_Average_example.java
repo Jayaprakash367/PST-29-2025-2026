589b093f0e0b8b292fb82e9febf39d8b98eca547
jayaprakash26 <157467388+Jayaprakash367@users.noreply.github.com>
2026-01-21 21:07:49 +0530

    Update Sum_Average_example.java

--- Scanner/Sum_Average_example.java
+++ Scanner/Sum_Average_example.java
@@ -6,15 +6,18 @@ public class Sum_Average_example {
 		Scanner sc=new Scanner(System.in);
 		 System.out.print("Enter the Size of the element:");
 		    int a=sc.nextInt();
-		    sc.nextLine();
-		    int arr[]=new int[a];
-		    System.out.println("Enter the number:");
-		    int Sum=0;
-		    for(int i=0; i<=a; i++) {
-		    	arr[i]=sc.nextInt(i);
-		    	Sum+=arr[i];
-		    			
-		    }
-	}
+			int arr[] = new int[a];
+			System.out.println("Enter the numbers:");
+			int Sum = 0;
+			for (int i = 0; i < a; i++) {
+				System.out.print("Element " + (i + 1) + ": ");
+				arr[i] = sc.nextInt();
+				Sum += arr[i];
+			}
 
+			System.out.println("Sum of the elements: " + Sum);
+			System.out.println("Average of the elements: " + ((double) Sum / a));
+			sc.close();
+	}
 }
+
