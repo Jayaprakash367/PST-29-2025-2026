44a5287ae37a3f10f8465a1ea245935a068e1687
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-01-28 18:27:42 +0530

    Add files via upload

---
+++ CORE_JAVA_EXAMPLE/Sum_of_Array_elements.java
@@ -0,0 +1,25 @@
+package Java_program;
+import java.util.Scanner;
+public class Sum_of_Array_elements {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the number of elements Size: ");
+		int n = scanner.nextInt();
+		
+		int[] array = new int[n];
+		System.out.println("Enter the elements of the array:");
+		for (int i = 0; i < n; i++) {
+			array[i] = scanner.nextInt();
+		}
+		
+		int sum = 0;
+		for (int num : array) {
+			sum += num;
+		}
+		System.out.println("The sum of the array elements is: " + sum);
+		scanner.close();
+ 
+	}
+
+}
