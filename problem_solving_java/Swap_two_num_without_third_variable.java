fa7ab0005b5a5cf11292187bb1d09bfe50c33062
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-01-29 21:10:37 +0530

    Add files via upload

---
+++ problem_solving_java/Swap_two_num_without_third_variable.java
@@ -0,0 +1,18 @@
+package Core_java_program;
+import java.util.Scanner;
+public class Swap_two_num_without_third_variable {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number:");
+		int a = sc.nextInt();
+		System.out.println("Enter second number:");
+		int b=sc.nextInt();
+		System.out.println("Before swapping: a = " + a + ", b = " + b);
+		a = a + b; 
+		b = a - b; 
+		a = a - b; 
+		System.out.println("After swapping: a = " + a + ", b = " + b);
+	}
+
+}
