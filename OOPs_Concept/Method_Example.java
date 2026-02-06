44c637f63fd0536d73f7f476ec3d30271b4ffef1
jayaprakash26 <jayaprakashkumar367@gmail.com>
2026-02-06 21:01:16 +0530

    Add files via upload

---
+++ OOPs_Concept/Method_Example.java
@@ -0,0 +1,17 @@
+package OOPs_Concept;
+import java.util.Scanner;
+public class Method_Example {
+	public static void main(String[] args) {
+		Method();
+	}
+	public static void Method() {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the value of a:");
+		int a=scanner.nextInt();
+		System.out.print("Enter the value of a:");
+		int b=scanner.nextInt();
+		int sum=a+b;
+		System.out.println("The Value of a and b is :"+sum);
+       scanner.close();
+}
+}
